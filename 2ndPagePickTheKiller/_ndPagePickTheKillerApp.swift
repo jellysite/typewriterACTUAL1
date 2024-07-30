c179d22fd43f8a2d36c423c4c4a9d5cf85ae94bf
Leonie <leoniele1990@gmail.com>
2024-07-30 11:55:18 +0100

    TYPE WRITING EFFECT HELL YEAH

---
+++ 2ndPagePickTheKiller/_ndPagePickTheKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ndPagePickTheKillerApp.swift
+//  2ndPagePickTheKiller
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct _ndPagePickTheKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 2ndPagePickTheKiller/typewriter.swift
@@ -0,0 +1,18 @@
+//
+//  typewriter.swift
+//  2ndPagePickTheKiller
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+struct typewriter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    typewriter()
+}
